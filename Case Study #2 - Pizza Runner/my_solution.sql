9d0f64e5f9303a041803c0820e857320c758cda6
Niv Shir <80172576+Nivshiz@users.noreply.github.com>
2022-10-09 10:33:38 +0300

    Update my_solution.sql

--- Case Study #2 - Pizza Runner/my_solution.sql
+++ Case Study #2 - Pizza Runner/my_solution.sql
@@ -1,1 +1,7 @@
-/* select */
+/* The first thing i did was investigate the two problematic tables - customer_orders and runner_orders as indicated in the description.
+I found that the values that require update are: "", "null" and NULL, in both tables.
+*/
+
+-- I encountered a warning during my effort to update the tables, this SET statement helped solve it:
+SET SQL_SAFE_UPDATES = 0;
+
