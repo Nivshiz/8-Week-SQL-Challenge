a8a194c7057aa218c243cb34c05fae0c608449eb
Niv Shir <80172576+Nivshiz@users.noreply.github.com>
2022-10-09 10:59:20 +0300

    Update my_solution.sql

--- Case Study #2 - Pizza Runner/my_solution.sql
+++ Case Study #2 - Pizza Runner/my_solution.sql
@@ -1,12 +1,19 @@
 /* 
-The first thing i did was investigate the two problematic tables - customer_orders and runner_orders as indicated in the description.
-I used SELECT DISTINCT for the unique values in each column, and I found that the values that require update are: "", "null" and NULL, in both tables. 
+The first thing i did was to investigate the two problematic tables - customer_orders and runner_orders as indicated in the description.
+
+For the customer_orders table, I used SELECT DISTINCT for the unique values in each column, and I found that the values that require update are: "", "null" and NULL.
 I decided to replace those values with a 0 value.
+
+For the runner_orders table, in addition to the same values in the customer_orders table, 
+I found that the distance column contains string values like "km" that need to be removed. 
+Furthermore, the duration column contains various strings that need to be removed (minutes, minutes, minutes, etc.).
 */
 
 -- I encountered a warning during my effort to update the tables, this SET statement helped solve it:
 SET SQL_SAFE_UPDATES = 0;
 
+
+-- customer_orders table
 -- These UPDATE statements used to replace the problematic values into 0 values
 UPDATE customer_orders SET exclusions=0
 WHERE exclusions IS NULL OR exclusions IN ('', 'null');
@@ -14,3 +21,14 @@ WHERE exclusions IS NULL OR exclusions IN ('', 'null');
 UPDATE customer_orders SET extras=0
 WHERE extras IS NULL OR extras IN ('', 'null');
 
+
+-- runner_orders table
+-- replacing problematic values into 0 values
+UPDATE customer_orders SET exclusions=0
+WHERE exclusions IS NULL OR exclusions IN ('', 'null');
+
+UPDATE customer_orders SET extras=0
+WHERE extras IS NULL OR extras IN ('', 'null');
+
+UPDATE customer_orders SET extras=0
+WHERE extras IS NULL OR extras IN ('', 'null');
