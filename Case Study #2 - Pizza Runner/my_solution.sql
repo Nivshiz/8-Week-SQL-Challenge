dfcdd6dfc6df52001f19f2a015066e0033dd07f9
Niv Shir <80172576+Nivshiz@users.noreply.github.com>
2022-10-10 11:11:44 +0300

    Update my_solution.sql

--- Case Study #2 - Pizza Runner/my_solution.sql
+++ Case Study #2 - Pizza Runner/my_solution.sql
@@ -26,10 +26,41 @@ WHERE extras IS NULL OR extras IN ('', 'null');
 
 -- runner_orders table
 
--- replacing all missing values into NULL values in the cancellation column
+-- replacing all missing values into NULL values in the cancellation and pickup_time columns
 UPDATE runner_orders SET cancellation = NULL
 WHERE cancellation IN ('', 'null');
+
+UPDATE runner_orders SET pickup_time = NULL
+WHERE pickup_time IN ('', 'null');
 
 -- deleting all unwanted chars in the distance and duration columns
 UPDATE runner_orders SET duration = REGEXP_SUBSTR(duration, "[0-9]+"), 
   distnace = REGEXP_SUBSTR(distance, "[0-9]+");
+
+
+
+-- Questions
+
+-- A. Pizza Metrics
+
+-- 1. total ordered pizzas
+SELECT COUNT(*) AS total_orders FROM customer_orders;
+
+-- 2. unique customer orders
+SELECT COUNT(DISTINCT order_id) AS unique_customer_orders FROM customer_orders;
+
+-- 3. successful orders that were delivered by each runner
+SELECT runner_id, COUNT(*) AS orders_count
+FROM runner_orders
+WHERE cancellation IS NULL
+GROUP BY runner_id;
+
+-- 4. total count of each type of pizza delivered
+SELECT pizza_id, COUNT(*) AS total_delivered
+FROM customer_orders
+INNER JOIN runner_orders
+	ON customer_orders.order_id = runner_orders.order_id
+WHERE cancellation IS NULL
+GROUP BY pizza_id;
+
+-- 5. Vegetarian and Meatlovers that were ordered by each customer
